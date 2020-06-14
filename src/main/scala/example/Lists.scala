d2d9ed4342c1fdd5724d14cd14d05c094ba2a00a
Arthur Almeida <arthur.almeidapereira@gmail.com>
2020-06-14 20:36:52 -0300

    fix broken tests. Implements sum logic.

--- src/main/scala/example/Lists.scala
+++ src/main/scala/example/Lists.scala
@@ -22,7 +22,10 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int = {
+    if (!xs.isEmpty) xs.head + sum(xs.tail)
+    else 0
+  }
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -37,5 +40,9 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  // def max(xs: List[Int]): Int = {
+  //   if (xs.isEmpty) thrown new NoSuchElementException();
+
+
+  // }
 }
